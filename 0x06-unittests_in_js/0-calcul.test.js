a942c83363bcdc0eb88384ac5c54d52715f1419c
niialabi <taufiq.alabi@gmail.com>
2024-05-02 19:40:36 +0000

    eslint fix try

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,21 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-    it('checks whole numbers', function() {
-        const result = calculateNumber(1, 3)
-        assert.equal(result, 4);
-    })
-    it('checks whole and decimal', function() {
-        const result = calculateNumber(1, 3.7)
-        assert.equal(result, 5);
-    })
-    it('checks decimal and decimal', function() {
-        const result = calculateNumber(1.2, 3.7)
-        assert.equal(result, 5);
-    })
-    it('checks decimal2 and decimal2', function() {
-        const result = calculateNumber(1.5, 3.7)
-        assert.equal(result, 6);
-    })
+describe('calculateNumber', function () {
+  it('checks whole numbers', function () {
+    const result = calculateNumber(1, 3)
+    assert.equal(result, 4);
+  })
+  it('checks whole and decimal', function () {
+    const result = calculateNumber(1, 3.7)
+    assert.equal(result, 5);
+  })
+  it('checks decimal and decimal', function () {
+    const result = calculateNumber(1.2, 3.7)
+    assert.equal(result, 5);
+  })
+  it('checks decimal2 and decimal2', function () {
+    const result = calculateNumber(1.5, 3.7)
+    assert.equal(result, 6);
+  })
 });
